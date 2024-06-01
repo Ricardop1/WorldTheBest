eaecb07da8ea93fdaa7a0cef0802392af0e31f10
Ricardop1 <47947522+Ricardop1@users.noreply.github.com>
2024-06-01 10:44:32 +0200

    Add CSV and plots

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 import streamlit as st
 import pandas as pd
-
+import matplotlib.pyplot as plt
 st.title('WORLD THE BEST')
 
-pd.read_csv("./thebest.csv")
+df = pd.read_csv("./thebest.csv")
+
+
+# Group by the 'equipo' column
+grouped_equipo_df = df.groupby('equipo').sum()  # or any other aggregation function
+
+# Create a plot
+fig, ax = plt.subplots()
+grouped_df.plot(kind='bar', ax=ax)
+ax.set_title('Grouped by Equipo')
+ax.set_xlabel('Equipo')
+ax.set_ylabel('Values')  # change this to the appropriate label
+
+st.pyplot(fig)
+
+
+# Group by the 'equipo' column
+grouped_equipo_df = df.groupby('entrenador').sum()  # or any other aggregation function
+
+# Create a plot
+fig, ax = plt.subplots()
+grouped_df.plot(kind='bar', ax=ax)
+ax.set_title('Grouped by Entrenador')
+ax.set_xlabel('Entrenador')
+ax.set_ylabel('Values')  # change this to the appropriate label
+
+st.pyplot(fig)
