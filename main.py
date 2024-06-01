0aadfeddbad33ac6b60b6a6ede338120d1845c32
Ricardop1 <47947522+Ricardop1@users.noreply.github.com>
2024-06-01 10:49:03 +0200

    fix grouped

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ grouped_equipo_df = df.groupby('equipo').sum()  # or any other aggregation funct
 
 # Create a plot
 fig, ax = plt.subplots()
-grouped_df.plot(kind='bar', ax=ax)
+grouped_equipo_df.plot(kind='bar', ax=ax)
 ax.set_title('Grouped by Equipo')
 ax.set_xlabel('Equipo')
 ax.set_ylabel('Values')  # change this to the appropriate label
@@ -20,11 +20,11 @@ st.pyplot(fig)
 
 
 # Group by the 'equipo' column
-grouped_equipo_df = df.groupby('entrenador').sum()  # or any other aggregation function
+grouped_entrenador_df = df.groupby('entrenador').sum()  # or any other aggregation function
 
 # Create a plot
 fig, ax = plt.subplots()
-grouped_df.plot(kind='bar', ax=ax)
+grouped_entrenador_df.plot(kind='bar', ax=ax)
 ax.set_title('Grouped by Entrenador')
 ax.set_xlabel('Entrenador')
 ax.set_ylabel('Values')  # change this to the appropriate label
